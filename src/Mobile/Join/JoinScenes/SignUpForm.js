ed74d569c03fafc040d46d6f4a4f2ad109d31327
Will Johnston <wijohnst@gmail.com>
2020-05-03 09:54:41 -0400

    form troubleshoot

--- src/Mobile/Join/JoinScenes/SignUpForm.js
+++ src/Mobile/Join/JoinScenes/SignUpForm.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 import SignUpForm2 from './SignUpForm2'
 
-const SignUpFormWrapper = styled.div`
+const SignUpFormWrapper = styled.form`
   background-color: white;
   border-radius: 5px;
   padding: 5px;
@@ -12,42 +12,33 @@ const SignUpFormWrapper = styled.div`
 `
 export default function SignUpForm() {
 
-  const [isService, setIsService] = useState(null);
-  const [isSubmitted, setIsSubmitted] = useState(false);
+  // const [isService, setIsService] = useState(null);
+  // const [isSubmitted, setIsSubmitted] = useState(false);
 
-  let handleSelect = (value) =>{
-    setIsService(value);
-  }
+  // let handleSelect = (value) =>{
+  //   setIsService(value);
+  // }
 
-  if(!isSubmitted){
   return (
-    <React.Fragment>
-      <SignUpFormWrapper name='sign-up' method='POST'>
-        <input type="hidden" name="sign-up" value="sign-up" />
-        <p>
-          <label>First Name: <input type='text' name='first-name' /></label>
-        </p>
-        <p>
-          <label>Last Name: <input type='text' name='last-name' /></label>
-        </p>
-        <p>
-          <label>Email: <input type='email' name='email' /></label>
-        </p>
-        <p> Do you typically work in the service industry?</p>
-        <input type='radio' name='is_service' id='is_service_true' value='true' onChange={() => handleSelect(true)}/>
-          <label> Yes </label>
-        <input type='radio' name='is_service' id='is_service_false' value='false' onChange={() => handleSelect(false)}/>
-          <label> No </label>
-        {isService === null ? <p /> : <SignUpForm2 isService={isService} form={'sign-up'}/>}<br />
-        {/* <button type='submit' onClick={() => setIsSubmitted(true)}>Submit</button> */}
-        <button type='submit'>Submit</button>
-      </SignUpFormWrapper>
-    </React.Fragment>
-    )
-  }
-  else{
-    return(
-      <p>Success</p>
-    )
-  }
+    <SignUpFormWrapper name='sign-up' method='post'>
+      <input type="hidden" name="sign-up" value="sign-up" />
+      <p>
+        <label>First Name: <input type='text' name='first-name' /></label>
+      </p>
+      <p>
+        <label>Last Name: <input type='text' name='last-name' /></label>
+      </p>
+      <p>
+        <label>Email: <input type='email' name='email' /></label>
+      </p>
+      {/* <p> Do you typically work in the service industry?</p>
+      <input type='radio' name='is_service' id='is_service_true' value='true' onChange={() => handleSelect(true)}/>
+        <label> Yes </label>
+      <input type='radio' name='is_service' id='is_service_false' value='false' onChange={() => handleSelect(false)}/>
+        <label> No </label>
+      {isService === null ? <p /> : <SignUpForm2 isService={isService} form={'sign-up'}/>}<br />
+      <button type='submit' onClick={() => setIsSubmitted(true)}>Submit</button> */}
+      <button type='submit'>Submit</button>
+    </SignUpFormWrapper>
+  )
 }
