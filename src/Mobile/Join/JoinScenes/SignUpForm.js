9c12a046bd48294ed62610cf014ae4d78068cef1
Will Johnston <wijohnst@gmail.com>
2020-05-03 10:28:58 -0400

    form refactor for fetch

--- src/Mobile/Join/JoinScenes/SignUpForm.js
+++ src/Mobile/Join/JoinScenes/SignUpForm.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import styled from 'styled-components'
 
-import SignUpForm2 from './SignUpForm2'
+// import SignUpForm2 from './SignUpForm2'
 
 const SignUpFormWrapper = styled.form`
   background-color: white;
@@ -13,32 +13,52 @@ const SignUpFormWrapper = styled.form`
 export default function SignUpForm() {
 
   // const [isService, setIsService] = useState(null);
-  // const [isSubmitted, setIsSubmitted] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
-  // let handleSelect = (value) =>{
-  //   setIsService(value);
-  // }
+  const [firstName, setFirstName] = useState('');
+  const[lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
 
+  const encode = (data) =>{
+    const formData = new FormData();
+    Object.keys(data).forEach((k) =>{
+      formData.append(k,data[k])
+    });
+    return formData;
+  }
+  const handleSubmit = e =>{
+    console.log('Submitted...')
+    setIsSubmitted(true);
+
+    const formData = {contactInfo : lastName, firstName, email};
+
+    fetch('/', {
+      method: 'POST',
+      body: encode(formData)
+    })
+    .then(()=> setIsSubmitted(true))
+    .catch(error => console.log(error));
+    
+    e.preventDefault();
+  }
+  
+
+  if(!isSubmitted){
   return (
-    <SignUpFormWrapper name='sign-up' method='post'>
-      <input type="hidden" name="sign-up" value="sign-up" />
-      <p>
-        <label>First Name: <input type='text' name='first-name' /></label>
-      </p>
-      <p>
-        <label>Last Name: <input type='text' name='last-name' /></label>
-      </p>
-      <p>
-        <label>Email: <input type='email' name='email' /></label>
-      </p>
-      {/* <p> Do you typically work in the service industry?</p>
-      <input type='radio' name='is_service' id='is_service_true' value='true' onChange={() => handleSelect(true)}/>
-        <label> Yes </label>
-      <input type='radio' name='is_service' id='is_service_false' value='false' onChange={() => handleSelect(false)}/>
-        <label> No </label>
-      {isService === null ? <p /> : <SignUpForm2 isService={isService} form={'sign-up'}/>}<br />
-      <button type='submit' onClick={() => setIsSubmitted(true)}>Submit</button> */}
-      <button type='submit'>Submit</button>
-    </SignUpFormWrapper>
-  )
+      <SignUpFormWrapper onSubmit={handleSubmit} >
+        <p>First name:</p>
+        <input type='text' name='firstName' onChange={(e) => setFirstName(e.target.value)} />
+        <p> Last name:</p>
+        <input type='text' name='lastName' onChange={(e) => setLastName(e.target.value)} />
+        <p>Email:</p>
+        <input type='email' name='email' onChange={(e) => setEmail(e.target.value)} />
+        <button type="submit"> Join CodeBurger!</button>
+      </SignUpFormWrapper>
+    )
+  }else{
+    return(
+      <p>Success!</p>
+    )
+  }
+
 }
